a8b49feebd916bfc30b5468d9337255c2a7fa858
MigueMartinRoman <miguel.amr83@gmail.com>
2023-12-13 12:04:52 +0100

    Filter 1 terminado

--- src/main/java/org/eclipse/jakarta/hello/interceptor/LoginInterceptor.java
+++ src/main/java/org/eclipse/jakarta/hello/interceptor/LoginInterceptor.java
@@ -3,6 +3,9 @@ package org.eclipse.jakarta.hello.interceptor;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -17,7 +20,19 @@ public class LoginInterceptor implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+
+        HttpSession httpSession = request.getSession();
+        String autenticado = (String)httpSession.getAttribute("autenticado");
+
+        if (autenticado!=null && autenticado.equals("SI")){
+            // Patrón: chain of responsability
+            filterChain.doFilter(servletRequest,servletResponse);
+        } else {
+            response.sendRedirect("login");
+        }
     }
 
     @Override
